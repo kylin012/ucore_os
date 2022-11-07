7bc1b9d89a0c569841056b1d5697d04f9c1dd0b1
NKULYX <1078346719@qq.com>
2022-11-07 20:41:12 +0800

    finish lab2 buddy_check

--- labcodes/lab2/kern/mm/buddy_pmm.c
+++ labcodes/lab2/kern/mm/buddy_pmm.c
@@ -21,7 +21,6 @@ int ROUND_UP_LOG(int size){
 }
 
 unsigned* buddy_manager;
-
 struct Page* page_base;
 
 int free_page_num, manager_size;
@@ -52,8 +51,8 @@ buddy_init_memmap(struct Page *base, size_t n){
     // 剩余可用页数
     free_page_num = n - 4 * manager_size / 4096;
     // buddy数组的下标[1 … manager_size]有效
-    int i = 1;
-    int node_size = manager_size / 2;
+    unsigned i = 1;
+    unsigned node_size = manager_size / 2;
     for(; i < manager_size; i++){
         buddy_manager[i] = node_size;
         if(IS_POWER_OF_2(i+1)){
@@ -83,7 +82,7 @@ buddy_alloc(int size){
     if(buddy_manager[index] < size)
         return -1;
     // 从根节点往下深度遍历，找到恰好等于size的块
-    for(node_size = buddy_manager[index];node_size != size;node_size /= 2){
+    for(node_size = manager_size / 2;node_size != size;node_size /= 2){
         if(buddy_manager[LEFT_LEAF(index)] >= size)
             index = LEFT_LEAF(index);
         else
@@ -92,7 +91,8 @@ buddy_alloc(int size){
     // 将找到的块取出分配
     buddy_manager[index] = 0;
     // 计算块在所有块内存中的索引
-    offset = (index + 1) * node_size - manager_size / 2;
+    offset = (index) * node_size - manager_size / 2;
+    cprintf(" index:%u offset:%u ", index, offset);
     // 向上回溯至根节点，修改沿途节点的大小
     while(index > 1){
         index = PARENT(index);
@@ -103,6 +103,7 @@ buddy_alloc(int size){
 
 static struct Page*
 buddy_alloc_pages(size_t n) {
+    cprintf("alloc %u pages", n);
     assert(n>0);
     if(n > free_page_num)
         return NULL;
@@ -118,32 +119,37 @@ buddy_alloc_pages(size_t n) {
     }
     free_page_num -= round_n;
     base->property = n;
+    cprintf("finish!\n");
     return base;
 }
 
 static void
 buddy_free_pages(struct Page* base, size_t n) {
+    cprintf("free  %u pages", n);
     //STEP1: 重置pages中对应的page
     assert(n > 0);
-    assert(PageReserved(base));	//保证base原先处于保留态
+    n = 1 << ROUND_UP_LOG(n);
+    assert(!PageReserved(base));	//保证base原先处于保留态
     for(struct Page* p = base; p < base + n; p++){
         assert(!PageReserved(p) && !PageProperty(p));//确认该帧已经被分配，且不是保留帧
         //在此作者（ZJK）认为：由于不需要property，将所有page的property flag置为0即可
         set_page_ref(p, 0);
     }
     //STEP2: 将buddy中的对应节点释放
-    //自底向上寻找
+    //自底向上寻找 
     unsigned offset = base - page_base;//开始块序号
     unsigned index = manager_size / 2 + offset;//对应叶节点
     unsigned node_size = 1;
-    while(buddy_manager[index]){//找第一个值为0的节点
+    while(node_size!=n){//找第一个值为0的节点
         index = PARENT(index);//上溯
         node_size *= 2;
         assert(index);//直到根节点都不为0，说明有误，中断
     }
     buddy_manager[index] = node_size;
+    cprintf(" index:%u offset:%u ", index, offset);
     //STEP3: 回溯直到根节点，更改沿途值
     index = PARENT(index);
+    node_size *= 2;
     while(index){
         unsigned leftSize = buddy_manager[LEFT_LEAF(index)];
         unsigned rightSize = buddy_manager[RIGHT_LEAF(index)];
@@ -151,14 +157,17 @@ buddy_free_pages(struct Page* base, size_t n) {
             buddy_manager[index] = node_size;
         }
         else if(leftSize>rightSize){
-            node_size = leftSize;
+            buddy_manager[index] = leftSize;
         }
         else{
-            node_size = rightSize;
+            buddy_manager[index] = rightSize;
         }
+        index = PARENT(index);
+        node_size *= 2;
     }
     //STEP4: 增加空闲页数，结束释放过程
     free_page_num += n;
+    cprintf("finish!\n");
 }
 
 static size_t
@@ -173,10 +182,50 @@ basic_check(void) {
 
 static void
 buddy_check(void) {
+    cprintf("buddy check!\n");
+    struct Page *p0, *A, *B, *C, *D;
+    p0 = A = B = C = D = NULL;
 
+    assert((p0 = alloc_page()) != NULL);
+    assert((A = alloc_page()) != NULL);
+    assert((B = alloc_page()) != NULL);
+
+    assert(p0 != A && p0 != B && A != B);
+    assert(page_ref(p0) == 0 && page_ref(A) == 0 && page_ref(B) == 0);
+
+    free_page(p0);
+    free_page(A);
+    free_page(B);
+
+    A = alloc_pages(512);
+    B = alloc_pages(512);
+    free_pages(A, 256);
+    free_pages(B, 512);
+    free_pages(A + 256, 256);
+
+    p0 = alloc_pages(8192);
+    assert(p0 == A);
+    // free_pages(p0, 1024);
+    //以下是根据链接中的样例测试编写的
+    A = alloc_pages(128);
+    B = alloc_pages(64);
+    assert(A + 128 == B);  //检查是否相邻
+    C = alloc_pages(128);
+    assert(A + 256 == C);  //检查C有没有和A重叠
+    free_pages(A, 128);  //释放A
+    D = alloc_pages(64);
+    cprintf("D %p\n", D);
+    assert(D + 128 == B);  //检查D是否能够使用A刚刚释放的内存
+    free_pages(C, 128);
+    C = alloc_pages(64);
+    assert(C == D + 64 && C == B - 64);  // 检查C是否在B、D之间
+    free_pages(B, 64);
+    free_pages(D, 64);
+    free_pages(C, 64);
+    free_pages(p0, 8192);  //全部释放
 }
 
-const struct pmm_manager default_pmm_manager = {
+const struct pmm_manager buddy_pmm_manager = {
     .name = "buddy_pmm_manager",
     .init = buddy_init,
     .init_memmap = buddy_init_memmap,
